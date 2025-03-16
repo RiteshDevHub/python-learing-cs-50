db7c6255108ab1b67640ca60d74adc8531e70b6b
RiteshDevHub <riteshux@gmail.com>
2025-03-16 23:11:13 +0530

    _

--- professor.py
+++ professor.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 import random
-
 
 def main():
     level = get_level("Level: ")
     problems = generate_integer(level)
     solution = solve(problems)
-
 
 def get_level(prompt):
     while True:
@@ -18,7 +15,6 @@ def get_level(prompt):
                 raise ValueError
         except ValueError:
             pass
-
 
 def generate_integer(level):
     prob = {}
@@ -67,74 +63,4 @@ def solve(que):
     return
 
 if __name__ == "__main__":
-=======
-import random
-
-
-def main():
-    level = get_level("Level: ")
-    problems = generate_integer(level)
-    solution = solve(problems)
-
-
-def get_level(prompt):
-    while True:
-        try:
-            n = int(input(prompt))
-            if 1 <= n <= 3:
-                return n
-            else:
-                raise ValueError
-        except ValueError:
-            pass
-
-
-def generate_integer(level):
-    prob = {}
-    if level == 1:
-        for _ in range(10):
-            x = random.randint(0,9)
-            y = random.randint(0,9)
-            prob[f"{x} + {y} ="] = x + y
-        return prob
-    elif level == 2:
-        for _ in range(10):
-            x = random.randint(10,99)
-            y = random.randint(10,99)
-            prob[f"{x} + {y} ="] = x + y
-        return prob
-    else:
-        for _ in range(10):
-            x = random.randint(100,999)
-            y = random.randint(100,999)
-            prob[f"{x} + {y} ="] = x + y
-        return prob
-
-def solve(que):
-    correct_ans = 0
-    for q in que:
-        re_try = 0
-        while True:
-            try:
-                print(q, end = "")
-                ans = int(input(""))
-                if ans == que[q]:
-                    correct_ans += 1
-                    break
-                else:
-                    print ("EEE")
-                    re_try += 1
-                    raise KeyError
-            except KeyError:
-                if re_try == 3:
-                    print(q, que[q])
-                    re_try = 0
-                    break
-                pass
-
-    print("Score:", correct_ans)
-    return
-
-if __name__ == "__main__":
->>>>>>> 0da4643af6bd04b3573c0d72ea37ac49b9cbbd15
     main()
